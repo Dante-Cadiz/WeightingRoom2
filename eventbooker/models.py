c5d2501c5530b9c627d3cfd66136b7e0b9ec261f
Dante-Cadiz <dantecadiz98@mail.com>
2022-07-18 14:03:58 +0000

    add Event model and basic admin register for it

--- eventbooker/models.py
+++ eventbooker/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+STATUS = ((0, 'Draft'), (1, 'Upcoming'), (2, 'Past'))
+
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    slug = models.SlugField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name="events", null=True)
+    featured_image = CloudinaryField('image', default='placeholder')
+    created_on = models.DateTimeField(auto_now_add=True)
+    attendees = models.ManyToManyField(User, related_name='attendees', blank=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    def __str__(self):
+        return self.title
+
+#class Booking(models.Model):
