122a1f6e9fb80bbdfee814c207c02dba920b0a5d
Dante-Cadiz <dantecadiz98@mail.com>
2022-07-19 15:56:19 +0000

    add base.html and index.html to templates, createview for event lisst and urls.py file in eventbooker folder

--- eventbooker/models.py
+++ eventbooker/models.py
@@ -11,10 +11,10 @@ class Event(models.Model):
     author = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name="events", null=True)
     featured_image = CloudinaryField('image', default='placeholder')
     created_on = models.DateTimeField(auto_now_add=True)
+    scheduled_time = models.DateTimeField(null=True)
     attendees = models.ManyToManyField(User, related_name='attendees', blank=True)
     status = models.IntegerField(choices=STATUS, default=0)
 
     def __str__(self):
         return self.title
 
-#class Booking(models.Model):
